dd117985dea699be7ddf14c3e6bdf4f407bb8da6
Gregor Ronniger <g.ronniger@tu-berlin.de>
2022-03-27 16:46:13 +0200

    dumping old generate_utils.jl

--- src/generate_utils.jl
+++ src/generate_utils.jl
@@ -1,4 +1,4 @@
-using RubiSymbolics: syntax_repl_dict
+using RubiSymbolics: syntax_repl_dict, function_repl_dict, identifier
 
 rubi_utils_file = joinpath(@__DIR__, "../Rubi/IntegrationUtilityFunctions.m")
 utils_file = joinpath(@__DIR__, "utils.jl")
@@ -8,18 +8,29 @@ orig_file_content = open(rubi_utils_file) do rf
     read(rf, String)
 end
 out_file_content = replace(orig_file_content,
+    "lst[[1,1]][[1]]" => "Part(Part(lst, 1, 1), 1)",
+    "Reap[u][[2]]" => "Part(Reap(u), 2)",
+    "(lst=MonomialFactor[u,x])[[1]]" => "Part(lst=MonomialFactor(u, x), 1)",
     syntax_repl_dict...,
-    # ":=" => "=",
     )
 out_file_content = replace(out_file_content,
-    r"(?<function>\w+)::usage\h*=\h*\"(?<doc>.*)\";" => s"\"\g<doc>\"\nfunction \g<function> end",
-    # r"\((?<var>[a-zA-Z_]+[a-zA-Z0-9_]*)?_(?<type>\w+)\)" => s"(\g<var>::\g<type>)",
-    # r"\((?<var>[a-zA-Z_]+[a-zA-Z0-9_]*)?_\)" => s"(\g<var>)",
+    function_repl_dict...,
     )
-
+out_file_content = replace(out_file_content,
+    "=#*" => "=#",
+    "*#=" => "#=",
+    r"(?<function>\w+)::usage\h*=[\h\n]*\"(?<doc>.*?)\";"s => s"\"\g<doc>\"\nfunction \g<function> end",
+    "FalseQ(false) = true" => "FalseQ(b::Bool) = !b",
+    )
+out_file_content = replace(out_file_content,
+    r"(\n(?!\n|$))"   => "\n"*" "^4,    # indent
+    r"^(?!\n)"      => " "^4,
+    )
 touch(utils_file)
 open(utils_file, "w") do f
+    write(f, "@util_collect begin\n")
     write(f, out_file_content)
+    write(f, "end\n")
 end
 
 # when function(x^2+y...) -> place rule macro in front
@@ -27,3 +38,5 @@ end
 # others: just functions to call
 
 # @rule_ Module @utils Int(...) := ...
+
+# @register_symbolic lhs of := and define rule
